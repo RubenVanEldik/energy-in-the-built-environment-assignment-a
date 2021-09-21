8aef8207e141a16b731b47c36861e726152d0dd9
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2021-09-21 12:04:37 +0200

    Add a create_histogram function

--- src/init.py
+++ src/init.py
@@ -3,12 +3,16 @@
 import pandas as pd
 import pvlib
 import numpy as np
+import matplotlib.pyplot as plt
 import utils
+
+# Increase the padding between subplots
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
 # Define the location
 LATITUDE = 52.08746136865645
 LONGITUDE = 5.168080610130638
-
+MODELS = ['disc', 'dirint', 'dirindex', 'erbs']
 
 def calculate_dni(model, irradiance, solar_position):
     """Return a DNI series based on the model, irradiance, and solar position"""
@@ -37,6 +41,17 @@ def calculate_dni(model, irradiance, solar_position):
         return pvlib.irradiance.erbs(ghi, zenith, time).dni
     raise Exception('Invalid GHI-DNI model type')
 
+def create_histogram(irradiance):
+    for index, model in enumerate(MODELS):
+        # Get the DNI error series
+        dni_measured = irradiance.DNI
+        dni_calculated = irradiance['dni_' + model]
+        dni_error = dni_calculated - dni_measured
+
+        # Create a subplot and set the model as title
+        plt.subplot(2, 2, index + 1)
+        ax = dni_error.plot(kind='hist', logy=True, bins=100)
+        ax.title.set_text(model)
 
 # Get the irradiance and position of the sun
 # TODO: check if dataset exists in UTC timezone.
@@ -50,7 +65,10 @@ solar_position = solar_position[solar_position.elevation > 4]
 irradiance = irradiance[irradiance.index.isin(solar_position.index)]
 
 # Calculate the different DNI's
-for model in ['disc', 'dirint', 'dirindex', 'erbs']:
+for model in MODELS:
     irradiance['dni_' + model] = calculate_dni(model, irradiance, solar_position)
     errors = utils.compare_series(irradiance.DNI, irradiance['dni_' + model])
     utils.print_object(errors, name=model, uppercase=True)
+    
+
+create_histogram(irradiance)
