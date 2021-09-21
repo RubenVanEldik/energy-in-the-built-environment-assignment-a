ab1a1e50db6e240b5ed00cd85441636f2b674622
raj0974722 <r.kumar@students.uu.nl>
2021-09-21 16:59:04 +0200

    New changes for Question 2
    added a function and POA parameters

--- src/dataprep.py
+++ src/dataprep.py
@@ -11,33 +11,33 @@ Created on Tue Sep 21 14:06:46 2021
 
 import pandas as pd
 
-to_skip_lines = range(0, 10)
-
-
-data = pd.read_csv('../input/KNMI_2019_hourly.txt', skiprows=to_skip_lines)
-del data['# STN'] #delere station column
-data.columns = ['date', 'HH', 'wind', 'temp', 'GHI']
-#print(data)
-
-#fix date-time index
-data.date = data.date.astype(str)
-data.HH = data.HH.apply( lambda x : str(x).zfill(2) )
-data.HH = data.HH.replace('24', '00')
-data['datetime'] = data.date + data.HH + '00'
-data['datetime'] = pd.to_datetime(data.datetime, format= '%Y%m%d%H%M')
-
-data.index = data.datetime
-data = data[['wind', 'temp', 'GHI']]
-
-#fix GHI units from GHI (j/cm2 to kw/m2) , temperature & wind (0.1 to 1)
-data.GHI = data.GHI * 2.77778
-data.wind = data.wind/10
-data.temp = data.temp/10
-
-#fix timezone UTC
-data.index = data.index.tz_localize('UTC')
-
-#export to new csv
-
-data.to_csv('../input/csvdata_KNMI_2019_hourly.csv', sep = ";")
-#newdata = pd.read_csv('../input/csvdata_KNMI_2019_hourly.csv', sep = ";")
+def getKNMIdata():
+    to_skip_lines = range(0, 10)
+    data = pd.read_csv('../input/KNMI_2019_hourly.txt', skiprows=to_skip_lines)
+    del data['# STN'] #delere station column
+    data.columns = ['date', 'HH', 'wind', 'temp', 'GHI']
+    #print(data)
+    
+    #fix date-time index
+    data.date = data.date.astype(str)
+    data.HH = data.HH.apply( lambda x : str(x).zfill(2) )
+    data.HH = data.HH.replace('24', '00')
+    data['datetime'] = data.date + data.HH + '00'
+    data['datetime'] = pd.to_datetime(data.datetime, format= '%Y%m%d%H%M')
+    
+    data.index = data.datetime
+    data = data[['wind', 'temp', 'GHI']]
+    
+    #fix GHI units from GHI (j/cm2 to kw/m2) , temperature & wind (0.1 to 1)
+    data.GHI = data.GHI * 2.77778
+    data.wind = data.wind/10
+    data.temp = data.temp/10
+    
+    #fix timezone UTC
+    data.index = data.index.tz_localize('UTC')
+    
+    #export to new csv
+    file_loc = '../input/csvdata_KNMI_2019_hourly.csv';
+    data.to_csv('../input/csvdata_KNMI_2019_hourly.csv', sep = ";")
+    return file_loc
+    #newdata = pd.read_csv('../input/csvdata_KNMI_2019_hourly.csv', sep = ";")
--- src/question2.py
+++ src/question2.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import pandas as pd
+import pvlib
+import dataprep
+from init import calculate_dni
 # Create 'table' with surface tilt and azimuth
 
 buildings = {
@@ -46,10 +49,30 @@ buildings = {
         
     }
 
-           
+LATITUDE = 53.224
+LONGITUDE = 5.752
+file = dataprep.getKNMIdata()
 
+print(file);
+KNMIdata = pd.read_csv(file,
+                         sep=";", index_col="datetime", parse_dates=True)
+
+solar_position = pvlib.solarposition.ephemeris(
+    KNMIdata.index, LATITUDE, LONGITUDE, temperature=KNMIdata.temp)           
+
+dirindex_dni = calculate_dni('dirindex', KNMIdata, solar_position)
+
+dirindex_dhi = KNMIdata['GHI'] - dirindex_dni
+
+POA = pvlib.irradiance.get_total_irradiance(90, 225, 
+                                            solar_position['zenith'], solar_position['azimuth'], 
+                                            dirindex_dni, KNMIdata['GHI'], dirindex_dhi, dni_extra=None, airmass=None, 
+                                            albedo=0.25, surface_type=None, model='isotropic', 
+                                            model_perez='allsitescomposite1990')
+poa_val = POA
+print(POA)
 # Calculating POA question 2.3
-for building in buildings:
-    for facade in building['facades']:
-        POA = pvlib.irradiance.get_total_irradiance(facade['tilt'], facade['azimuth'], solar_zenith, solar_azimuth, dni, ghi, dhi, dni_extra=None, airmass=None, albedo=0.25, surface_type=None, model='isotropic', model_perez='allsitescomposite1990', **kwargs)
-        print(POA)
+#for building in buildings:
+    #for facade in building:
+        
+        
