4520cc7e41c29a0bdb7561aa4dc656b79e6bb258
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2021-09-22 13:47:03 +0200

    Fix linting errors

--- src/dataprep.py
+++ src/dataprep.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#Preparing KNMI data for station:
+# Preparing KNMI data for station:
 # STN         LON(east)   LAT(north)  ALT(m)      NAME
-# 270         5.752       53.224      1.20        Leeuwarden 
+# 270         5.752       53.224      1.20        Leeuwarden
 
 import pandas as pd
+
 
 def get_knmi_data():
     # Import the CSV file and set the column names
     data = pd.read_csv('../input/knmi_raw.csv', skiprows=range(0, 10))
     data.columns = ['station', 'date', 'HH', 'wind', 'temp', 'GHI']
-    
+
     # Fix datetime index
     data.date = data.date.astype(str)
-    data.HH = data.HH.apply( lambda x : str(x).zfill(2) )
+    data.HH = data.HH.apply(lambda x: str(x).zfill(2))
     data.HH = data.HH.replace('24', '00')
     data['datetime'] = data.date + data.HH + '00'
-    data['datetime'] = pd.to_datetime(data.datetime, format= '%Y%m%d%H%M')
-    
+    data['datetime'] = pd.to_datetime(data.datetime, format='%Y%m%d%H%M')
+
     # Set the datetime as index and keep only the wind, temperature, and GHI column
     data.index = data.datetime
     data = data[['wind', 'temp', 'GHI']]
-    
+
     # Fix the units
-    data.GHI = data.GHI * 2.77778 # J/cm2 to kW/m2
-    data.wind = data.wind / 10 # 0.1m/s to m/s
-    data.temp = data.temp / 10 # 0.1m/s to m/s
-    
+    data.GHI = data.GHI * 2.77778  # J/cm2 to kW/m2
+    data.wind = data.wind / 10  # 0.1m/s to m/s
+    data.temp = data.temp / 10  # 0.1m/s to m/s
+
     # Fix timezone UTC
     data.index = data.index.tz_localize('UTC')
-    
+
     # Export to new csv
-    file_path = '../input/knmi_processed.csv';
-    data.to_csv(file_path, sep = ";")
+    file_path = '../input/knmi_processed.csv'
+    data.to_csv(file_path, sep=";")
     return file_path
--- src/question2.py
+++ src/question2.py
@@ -14,24 +14,22 @@ LONGITUDE = 5.752
 file = get_knmi_data()
 
 KNMIdata = pd.read_csv(file,
-                         sep=";", index_col="datetime", parse_dates=True)
+                       sep=";", index_col="datetime", parse_dates=True)
 
 solar_position = pvlib.solarposition.ephemeris(
-    KNMIdata.index, LATITUDE, LONGITUDE, temperature=KNMIdata.temp)           
+    KNMIdata.index, LATITUDE, LONGITUDE, temperature=KNMIdata.temp)
 
 dirindex_dni = calculate_dni('dirindex', KNMIdata, solar_position)
 
 dirindex_dhi = KNMIdata['GHI'] - dirindex_dni
 
-POA = pvlib.irradiance.get_total_irradiance(90, 225, 
-                                            solar_position['zenith'], solar_position['azimuth'], 
-                                            dirindex_dni, KNMIdata['GHI'], dirindex_dhi, dni_extra=None, airmass=None, 
-                                            albedo=0.25, surface_type=None, model='isotropic', 
+POA = pvlib.irradiance.get_total_irradiance(90, 225,
+                                            solar_position['zenith'], solar_position['azimuth'],
+                                            dirindex_dni, KNMIdata['GHI'], dirindex_dhi, dni_extra=None, airmass=None,
+                                            albedo=0.25, surface_type=None, model='isotropic',
                                             model_perez='allsitescomposite1990')
 poa_val = POA
 print(POA)
 # Calculating POA question 2.3
-#for building in buildings:
-    #for facade in building:
-        
-        
+# for building in buildings:
+# for facade in building:
