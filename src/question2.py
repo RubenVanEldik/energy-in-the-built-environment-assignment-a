cdbb23391463e15b4ff0cb6cdcdc19cc33b3c206
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2021-09-23 12:49:06 +0200

    Move the POA calculation to its own function

--- src/question2.py
+++ src/question2.py
@@ -18,6 +18,33 @@ irradiance = utils.get_irradiance(filename, latitude=LATITUDE, longitude=LONGITU
 irradiance['DNI'] = utils.calculate_dni('dirindex', irradiance, latitude=LATITUDE, longitude=LONGITUDE)
 irradiance['DHI'] = irradiance.GHI - irradiance.DNI
 
+def calculate_poa(tilt, azimuth, irradiance):
+    """
+    Calculate the total irradiance for a tilt, azimuth and irradiance
+
+    Parameters:
+        tilt (float or int): Tilt of surface
+        azimuth (float or int): Azimuth of surface
+        irradiance (DataFrame): DataFrame with the irradiance
+
+    Returns:
+        obj: Object with total, diffuse, and direct irradiance data
+    """
+    # Define local variables for the get_total_irradiance function
+    zenith = irradiance.solar_zenith
+    azimuth = irradiance.solar_azimuth
+    dni = irradiance.DNI
+    ghi = irradiance.GHI
+    dhi = irradiance.DHI
+    
+    poa = pvlib.irradiance.get_total_irradiance(tilt, azimuth, zenith, azimuth, dni, ghi, dhi)
+    return {
+        'total': poa.poa_global.sum() / 1000,
+        'diffuse': poa.poa_diffuse.sum() / 1000,
+        'direct': poa.poa_direct.sum() / 1000
+    }
+
+
 def get_poa_all_facades(buildings, irradiance):
     """
     Loop over all facades of all buildings and calculate the the irradiance for each hour
@@ -31,18 +58,10 @@ def get_poa_all_facades(buildings, irradiance):
     """
     for building in buildings:
         for facade in buildings[building]['facades']:
-            # Define local variables for the get_total_irradiance function
-            tilt = facade['tilt']
-            azimuth = facade['azimuth']
-            zenith = irradiance.solar_zenith
-            azimuth = irradiance.solar_azimuth
-            dni = irradiance.DNI
-            ghi = irradiance.GHI
-            dhi = irradiance.DHI
+            poa = calculate_poa(facade['tilt'], facade['azimuth'], irradiance)
+            facade['poa_total'] = poa['total']
+            facade['poa_diffuse'] = poa['diffuse']
+            facade['poa_direct'] = poa['direct']
             
-            poa = pvlib.irradiance.get_total_irradiance(tilt, azimuth, zenith, azimuth, dni, ghi, dhi)
-            facade['poa_total'] = poa.poa_global.sum() / 1000
-            facade['poa_diffuse'] = poa.poa_diffuse.sum() / 1000
-            facade['poa_direct'] = poa.poa_direct.sum() / 1000
             
 get_poa_all_facades(buildings, irradiance)
