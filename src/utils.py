e11025f30dfc237c69e62fce5007f4eec3067d8b
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2021-09-25 18:18:12 +0200

    Ensure that the vertical padding is correct for any number of rows

--- src/utils.py
+++ src/utils.py
@@ -88,7 +88,7 @@ def create_plot_with_subplots(rows, columns, *, xlabel, ylabel, sharex=True, sha
     width = 6.5 * columns ** (1/3)
     height = 5 * rows ** (1/3)
     figure, axes = plt.subplots(nrows=rows, ncols=columns, sharex=sharex, sharey=sharey, figsize=(width, height))
-    figure.subplots_adjust(wspace=0.05, hspace=0.35)
+    figure.subplots_adjust(wspace=0.05, hspace=0.15 * rows)
         
     # Set the labels on the outer x and y axis
     if(rows > 1 and columns > 1):
